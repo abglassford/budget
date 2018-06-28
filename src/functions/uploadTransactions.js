f74a4dfacf1e345452c5b6191ab3aacb2018524a
Kevin Durbin <kevindurb@gmail.com>
2018-06-28 13:07:56 -0600

    start implementing handler

--- src/functions/uploadTransactions.js
+++ src/functions/uploadTransactions.js
@@ -1,10 +1,8 @@
-import { jsonResponse } from './utils/response';
 import mongoose from './utils/db';
+import { handleMethod } from './utils/handler';
 
-export const handler = (event, _, callback) => {
-  console.log(event.body);
-
-  callback(null, jsonResponse({
+export const handler = handleMethod('POST', async (event) => {
+  return {
     status: 'OK',
-  }));
-};
+  };
+});
